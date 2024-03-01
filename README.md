579fe0da13c639bdfdc5791f2583decac11516df
Anshidtp <shaanshid07@gmail.com>
2024-03-01 20:38:45 +0530

    readme file updated

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # LectureLoom
 AI-Driven Lecture Note Generator from YouTube Videos
+
+## Technogies Used:
+    1. Google Gemini Pro
+    2. Youtube Transcipt Api
+    3. Streamlit
+
+### Steps To Run:
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/Anshidtp/LectureLoom.git
+```
+### 2.  Create a conda environment after opening the repository
+
+```bash
+conda create -n <env_name> python=3.10 -y
+```
+
+```bash
+conda activate <env_name>
+```
+
+
+### 3. Install the Requirements
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up you local host and port
+```
+
+
+
+
--- app.py
+++ app.py
@@ -4,20 +4,33 @@ from dotenv import load_dotenv
 load_dotenv() ## load all the nevironment variables
 import os
 import google.generativeai as genai
-
+from urllib.parse import urlparse, parse_qs
 from youtube_transcript_api import YouTubeTranscriptApi
+
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 prompt="""You are Yotube video summarizer. You will be taking the transcript text
-and summarizing the entire video and providing the important summary in points
-within 250 words. Please provide the summary of the text given here:  """
+and summarizing the entire video and providing the important summary in points. 
+Please provide the summary of the text given as following format: 
 
+- Topic  Title(Bold)Large size
+- summary with each topics """
+
+# To extract Videoid from youtube url
+def extract_video_id(url):
+    # Parse the URL
+    parsed_url = urlparse(url)
+    # Extract query part from the URL
+    query_string = parsed_url.query
+    # Parse query string to get video ID
+    video_id = parse_qs(query_string).get("v", [None])[0]
+    return video_id
 
 ## to get the transcript data from youtube video
-def extract_transcript_details(youtube_video_url):
+def extract_transcript_details(video_url):
     try:
-        video_id=youtube_video_url.split("=")[1]
+        video_id=extract_video_id(video_url)
         
         transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
 
@@ -38,17 +51,22 @@ def generate_Lecture(transcript_text,prompt):
     return response.text
 
 st.title("Lecture Loom")
-youtube_link = st.text_input("Enter YouTube Video Link:")
+yt_url = st.text_input("Enter YouTube Video Link:")
 
-if youtube_link:
-    video_id = youtube_link.split("=")[1]
+if yt_url:
+    video_id = extract_video_id(yt_url)
     print(video_id)
-    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+    st.image(f"https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg", use_column_width=True)
+    
+
 
 if st.button("Get Lecture Notes"):
-    transcript_text=extract_transcript_details(youtube_link)
+    transcript_text=extract_transcript_details(yt_url)
 
     if transcript_text:
         summary=generate_Lecture(transcript_text,prompt)
-        st.markdown("# Lecture Notes:")
+        st.markdown("Here is your Lecture Notes:")
         st.write(summary)
+
+
+
